496f34f07a582e48f96518932863df5fae9fd11b
Taiki Kanda <taiki@konnyak.com>
2021-07-10 11:29:49 +0900

    update notebook.

--- notebooks/notebook.py
+++ notebooks/notebook.py
@@ -6,18 +6,15 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.2
+#       jupytext_version: 1.11.3
 #   kernelspec:
-#     display_name: 'Python 3.9.5 64-bit (''python-data-project-template-iQRL-cf6-py3.9'':
-#       poetry)'
-#     name: python395jvsc74a57bd0547178f094375aa6a99ffd8434afbefe68694ee5e04c3ec98fa8d224070e847a
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 # %% [markdown]
 # ## notebook
-#
-# 1. read a file
-# 1. pre-process
 
 # %%
 import sys
@@ -25,7 +22,6 @@ sys.path.append('../src')
 
 import preprocess
 
-import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
@@ -39,6 +35,6 @@ df['c'] = df.apply(
 df.head()
 
 # %%
-fig, axes = plt.subplots(figsize=(6, 4))
+fig, axes = plt.subplots(figsize=(10, 4))
 df.plot.box(ax=axes)
-axes.grid()
+axes.grid();
